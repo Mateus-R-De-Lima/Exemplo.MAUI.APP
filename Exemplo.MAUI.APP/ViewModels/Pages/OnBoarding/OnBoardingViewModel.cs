922d497ae94dc33be412fc4023778246dc060d35
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-19 00:59:02 -0300

    feat - Criando primeira ViewModel e transferindo clique no botão.
    A ideia é separar a lógica de negócios da interface, permitindo que a View Model gerencie ações como validações e comunicação com a API.
    
    Obs: é importante que o BindingContext seja implemetentado, pois a view precisa saber qual é a ViewModel que será executada.

---
+++ Exemplo.MAUI.APP/ViewModels/Pages/OnBoarding/OnBoardingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace Exemplo.MAUI.APP.ViewModels.Pages.OnBoarding
+{
+    public class OnBoardingViewModel
+    {
+        public ICommand LoginWithEmailAndPasswordCommand { get; set; }
+
+        public OnBoardingViewModel()
+        {
+            LoginWithEmailAndPasswordCommand = new Command(LoginWithEmailAndPassword);
+        }
+        private void LoginWithEmailAndPassword()
+        {
+
+        }
+
+    }
+}
--- Exemplo.MAUI.APP/Views/Pages/OnBoarding/OnBoardingPage.xaml.cs
+++ Exemplo.MAUI.APP/Views/Pages/OnBoarding/OnBoardingPage.xaml.cs
@@ -1,3 +1,5 @@
+using Exemplo.MAUI.APP.ViewModels.Pages.OnBoarding;
+
 namespace Exemplo.MAUI.APP.Views.Pages.OnBoarding;
 
 public partial class OnBoardingPage : ContentPage
@@ -5,5 +7,7 @@ public partial class OnBoardingPage : ContentPage
 	public OnBoardingPage()
 	{
 		InitializeComponent();
-	}
+
+		BindingContext = new OnBoardingViewModel();
+	}  
 }
